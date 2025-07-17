fafab6c659f6312881baef7c4bb4abd04e35705c
0xFF336699 <0xFF336699@gmail.com>
2025-07-17 09:17:36 +0800

    Thu Jul 17 09:17:12     2025

---
+++ docs/_next/static/chunks/pages/index-274f174ac973f4c1.js
@@ -0,0 +1,108 @@
+(self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || []).push([[332],{
+
+/***/ 31697:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(94513);
+/* harmony import */ var _barrel_optimize_names_CircularProgress_CssBaseline_Grid2_mui_material__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(95771);
+/* harmony import */ var _barrel_optimize_names_CircularProgress_CssBaseline_Grid2_mui_material__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(95746);
+/* harmony import */ var _barrel_optimize_names_CircularProgress_CssBaseline_Grid2_mui_material__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(32055);
+/* harmony import */ var _mui_material_nextjs_v15_appRouter__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(77010);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(94285);
+/* harmony import */ var _home__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(97062);
+// import React from 'react';
+// import { Box, AppBar, Toolbar, Typography, Container } from '@mui/material';
+// import { mainModel } from '../libs/mc/main/MainModel';
+// import { MeetingRoom } from '@im/livekit';
+// import { IMOpenPageLink } from "@im/matrix";
+// import { AppShell, TabbarContainer } from '@app/ui';
+// const HomePage: React.FC = () => {
+//   return (
+//     <Box>
+//       <AppBar position="static">
+//         <Toolbar>
+//           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+//             Home
+//           </Typography>
+//         </Toolbar>
+//       </AppBar>
+//       <Container>
+//         <Box sx={{ my: 2 }}>
+//           <MeetingRoom
+//             roomName="test-room"
+//             userName={`user-${Math.random().toString(36).substring(2, 8)}`}
+//             livekitConfig={mainModel.appConfig.livekit}
+//           />
+//           <IMOpenPageLink />
+//         </Box>
+//       </Container>
+//     </Box>
+//   );
+// };
+// export default HomePage;
+
+
+
+// import i18n from '@src/i18nconf/i18nSetting';
+
+
+function Loading() {
+    return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(_barrel_optimize_names_CircularProgress_CssBaseline_Grid2_mui_material__WEBPACK_IMPORTED_MODULE_3__/* ["default"] */ .A, {
+        container: true,
+        spacing: 0,
+        direction: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        sx: {
+            minHeight: '100vh'
+        },
+        children: /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(_barrel_optimize_names_CircularProgress_CssBaseline_Grid2_mui_material__WEBPACK_IMPORTED_MODULE_4__/* ["default"] */ .A, {
+            color: "secondary"
+        })
+    });
+}
+function Index() {
+    return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(_mui_material_nextjs_v15_appRouter__WEBPACK_IMPORTED_MODULE_5__/* ["default"] */ .A, {
+        children: [
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(_barrel_optimize_names_CircularProgress_CssBaseline_Grid2_mui_material__WEBPACK_IMPORTED_MODULE_6__/* ["default"] */ .Ay, {}),
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(react__WEBPACK_IMPORTED_MODULE_1__.Suspense, {
+                fallback: /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(Loading, {}),
+                children: /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)(_home__WEBPACK_IMPORTED_MODULE_2__["default"], {})
+            })
+        ]
+    });
+}
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Index);
+
+
+/***/ }),
+
+/***/ 40367:
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+
+    (window.__NEXT_P = window.__NEXT_P || []).push([
+      "/",
+      function () {
+        return __webpack_require__(31697);
+      }
+    ]);
+    if(false) {}
+  
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ __webpack_require__.O(0, [262,299,898,230,120,62,636,593,792], () => (__webpack_exec__(40367)));
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ _N_E = __webpack_exports__;
+/******/ }
+]);
+//# sourceMappingURL=index-274f174ac973f4c1.js.map
